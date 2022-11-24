c1442ede03933cbc434340ec12cc6fe5f340f0dd
reshad21 <miakala7@gmail.com>
2022-11-24 12:28:55 +0600

    data change

--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -3,17 +3,17 @@ import React from 'react';
 const Category = () => {
     const categories = [
         {
-            id: 1,
+            _id: 1,
             name: 'LENOVO',
             picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjv10lWM7gBfiEsdj4ooYGH_34Tz4qqjV9Dg&usqp=CAU'
         },
         {
-            id: 2,
+            _id: 2,
             name: 'DELL',
             picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_c-gda5PREVRaly7HGFcGgGkSBPmGGDZNew&usqp=CAU'
         },
         {
-            id: 3,
+            _id: 3,
             name: 'ASUS',
             picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt87G_8rat0eVQjxJ89RxAt7X20Btz7C-wsQ&usqp=CAU'
         }
@@ -24,12 +24,12 @@ const Category = () => {
                 {
                     categories.map(category => {
                         return (
-                            <div key={category.id} className="card shadow-xl">
+                            <div key={category?._id} className="card shadow-xl">
                                 <figure className="px-7 pt-7">
-                                    <img src={category.picture} alt="Shoes" className="rounded-xl" />
+                                    <img src={category?.picture} alt="Shoes" className="rounded-xl" />
                                 </figure>
                                 <div className="card-body items-center text-center">
-                                    <h2 className="card-title">{category.name}</h2>
+                                    <h2 className="card-title">{category?.name}</h2>
                                     <p>If a dog chews shoes whose shoes does he choose?</p>
                                     <div className="card-actions">
                                         <button className="btn btn-primary">SEE ALL</button>

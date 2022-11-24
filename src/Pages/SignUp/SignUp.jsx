f15703b28f0bd2ffca222fd71c049e4b3f07a38c
reshad21 <miakala7@gmail.com>
2022-11-24 12:21:22 +0600

    google sign up complete

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,13 +1,17 @@
+import { GoogleAuthProvider } from 'firebase/auth';
 import React, { useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import toast from 'react-hot-toast';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../contexts/AuthProvider';
 
 const SignUp = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
-    const { createUser, updateUser } = useContext(AuthContext);
+    const { createUser, updateUser, googleSignIn } = useContext(AuthContext);
     const [signUpError, setSignUPError] = useState('')
+    const location = useLocation();
+    const navigate = useNavigate();
+
     const handleSignUp = (data) => {
         // console.log(data);
         setSignUPError('');
@@ -28,6 +32,19 @@ const SignUp = () => {
                 console.log(error)
                 setSignUPError(error.message)
             });
+    }
+
+    const from = location.state?.from?.pathname || '/';
+    const googleProvider = new GoogleAuthProvider();
+    const handleGoogleSignin = () => {
+        googleSignIn(googleProvider)
+            .then((result) => {
+                const user = result.user;
+                console.log(user);
+                navigate(from, { replace: true });
+            }).catch((error) => {
+                console.error(error);
+            })
     }
 
     return (
@@ -62,7 +79,7 @@ const SignUp = () => {
                 </form>
                 <p>Already have an account <Link className='text-secondary' to="/login">Please Login</Link></p>
                 <div className="divider">OR</div>
-                <button className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
+                <button onClick={handleGoogleSignin} className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
 
             </div>
         </div>

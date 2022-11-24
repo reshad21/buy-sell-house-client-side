d41272ccc09284d3f2ada0cd7763c161f041e6f8
reshad21 <miakala7@gmail.com>
2022-11-24 21:44:46 +0600

    sign up form change

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,31 +1,49 @@
-import { GoogleAuthProvider } from 'firebase/auth';
+import { GoogleAuthProvider, updateProfile } from 'firebase/auth';
 import React, { useContext, useState } from 'react';
-import { useForm } from 'react-hook-form';
-import toast from 'react-hot-toast';
+import { toast } from 'react-hot-toast';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { AuthContext } from '../contexts/AuthProvider';
+import { auth, AuthContext } from './../contexts/AuthProvider';
 
 const SignUp = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const { createUser, updateUser, googleSignIn } = useContext(AuthContext);
+    const { createUser, googleSignIn } = useContext(AuthContext);
     const [signUpError, setSignUPError] = useState('')
     const location = useLocation();
     const navigate = useNavigate();
+    const from = location.state?.from?.pathname || '/';
 
-    const handleSignUp = (data) => {
+    const handleSignUp = (e) => {
         // console.log(data);
         setSignUPError('');
-        createUser(data.email, data.password)
+
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        const role = form.role.value;
+
+        const userInfo = {
+            name,
+            email,
+            password,
+            role
+        }
+        console.log(userInfo);
+
+
+        createUser(email, password)
             .then(result => {
                 const user = result.user;
                 console.log(user);
                 toast.success('User Created Successfully.');
-                const userInfo = {
-                    displayName: data.name
-                }
-                updateUser(userInfo)
-                    .then(() => { })
-                    .catch(err => console.log(err));
+
+                updateProfile(auth.currentUser, {
+                    displayName: name
+                })
+                    .then(() => {
+                        navigate(from, { replace: true });
+                    })
+                    .catch((error) => { });
 
             })
             .catch(error => {
@@ -34,7 +52,6 @@ const SignUp = () => {
             });
     }
 
-    const from = location.state?.from?.pathname || '/';
     const googleProvider = new GoogleAuthProvider();
     const handleGoogleSignin = () => {
         googleSignIn(googleProvider)
@@ -51,32 +68,22 @@ const SignUp = () => {
         <div className='h-[800px] flex justify-center items-center'>
             <div className='w-96 p-7 border-2'>
                 <h2 className='text-xl text-center'>Sign Up</h2>
-                <form onSubmit={handleSubmit(handleSignUp)}>
+                <form onSubmit={handleSignUp}>
                     <div className="form-control w-full max-w-xs">
                         <label className="label"> <span className="label-text">Name</span></label>
-                        <input type="text" {...register("name", {
-                            required: "Name is Required"
-                        })} className="input input-bordered w-full max-w-xs" />
-                        {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+                        <input type="text" name='name' className="input input-bordered w-full max-w-xs" />
                     </div>
                     <div className="form-control w-full max-w-xs">
                         <label className="label"> <span className="label-text">Email</span></label>
-                        <input type="email" {...register("email", {
-                            required: true
-                        })} className="input input-bordered w-full max-w-xs" />
-                        {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
+                        <input type="text" name='email' className="input input-bordered w-full max-w-xs" />
                     </div>
                     <div className="form-control w-full max-w-xs">
                         <label className="label"> <span className="label-text">Password</span></label>
-                        <input type="password" {...register("password", {
-                            required: "Password is required",
-                            minLength: { value: 6, message: "Password must be 6 characters long" }
-                        })} className="input input-bordered w-full max-w-xs" />
-                        {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
+                        <input type="password" name='password' className="input input-bordered w-full max-w-xs" />
                     </div>
                     <div className="form-control">
                         <label className="label"> <span className="label-text">Select Category</span></label>
-                        <select {...register("role", { required: 'Please Select A category' })} className="select select-bordered w-full max-w-xs">
+                        <select name='role' className="select select-bordered w-full max-w-xs">
                             <option value='Buying'>Buying</option>
                             <option value='Selling'>Selling</option>
                         </select>

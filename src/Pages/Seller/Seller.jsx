1dc344d89407d03c47e6b9ac9ed30df38f8fd276
reshad21 <miakala7@gmail.com>
2022-11-25 20:37:19 +0600

    send product from client side

--- src/Pages/Seller/Seller.jsx
+++ src/Pages/Seller/Seller.jsx
@@ -24,6 +24,23 @@ const Seller = () => {
             description,
         }
         console.log(productInfo);
+
+        fetch('http://localhost:5000/products', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(productInfo),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('Success:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+
+
     }
     return (
         <div>

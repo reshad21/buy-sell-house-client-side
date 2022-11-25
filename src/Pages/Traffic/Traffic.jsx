f6730b67396f3b456e5ebdfd0af8a939645608c5
reshad21 <miakala7@gmail.com>
2022-11-25 10:42:06 +0600

    data show from db

--- src/Pages/Traffic/Traffic.jsx
+++ src/Pages/Traffic/Traffic.jsx
@@ -13,17 +13,50 @@ const Traffic = () => {
     });
 
     if (isLoading) {
-        return <>
-            <button type="button" class="bg-indigo-500 ..." disabled>
-                <svg class="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24"></svg>
-                Processing...
-            </button>
-        </>
+        return (
+            <div className='flex justify-center items-center'>
+                <button type="button" class="bg-indigo-500 ..." disabled>
+                    <svg class="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24"></svg>
+                    Processing...
+                </button>
+            </div>
+        )
     }
 
     return (
         <div>
-            <h2 className='text-3xl'>All Traffic of This site: {users.length}</h2>
+            <h2 className='text-3xl mb-5'>All Traffic of This site: {users.length}</h2>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Title</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users?.map((user, i) => {
+                                return (
+                                    <tr>
+                                        <th>{i + 1}</th>
+                                        <td>{user?.name}</td>
+                                        <td>{user?.email}</td>
+                                        <td>{user?.role}</td>
+                                        <td><button className='btn btn-sm'>Delete</button></td>
+                                    </tr>
+                                )
+                            })
+                        }
+
+
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
